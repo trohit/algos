75bf37c9b7c17a75ee0bb02efcffd518221b789a
Rohit <trohit@users.noreply.github.com>
2017-05-02 08:04:46 +0530

    Create countbits.c

---
+++ countbits.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int countbits(int n)
+{
+        int i;
+        int setbits = 0;
+        for (i = 0; i < sizeof(int)*8; i++) {
+                int mask = 1 << i;
+                int res = n & ( 1<<i);
+                if (res)
+                        setbits++;
+        }
+        return setbits;
+}
+
+int main()
+{
+        int num;
+        printf("Enter a num: ");
+        scanf("%u", &num);
+        printf("bits set: %u\", countbits(num));
+        return 0;
+}
